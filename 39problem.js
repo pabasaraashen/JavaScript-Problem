3ff3938b48e02e77edc136b4b30617c8ed7924e6
pabasaraashen <135622609+pabasaraashen@users.noreply.github.com>
2024-10-27 22:30:02 +0530

    Create 39problem.js

---
+++ 39problem.js
@@ -0,0 +1,6 @@
+//Write a function that calculates the factorial of a given number.
+function factorial(n) {
+    if (n === 0) return 1;
+    return n * factorial(n - 1);
+}
+console.log(factorial(5)); // 120
